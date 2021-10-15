5d102d72341705340b5ae9e20a761e319602c0f7
Gizat <gizat.uteev@gmail.com>
2021-10-15 21:30:00 +0300

    Update place.js

--- place.js
+++ place.js
@@ -42,48 +42,28 @@ const loadPlaces = function (coords) {
     return Promise.resolve(PLACES);
 };
 
-
 window.onload = () => {
     const scene = document.querySelector('a-scene');
     return navigator.geolocation.getCurrentPosition(function (position) {
         getPlaces(position.coords)
             .then((places) => {
                 places.forEach((place) => {
-    showMessage("Start get geopos");
-    
+                    console.log(place);
+                    const latitude = place.lat;
+                    const longitude = place.lng;
 
-    // first get current user location
-            showMessage("geops was getted");
-         showMessage(
-                        "acc:" + position.coords.accuracy +
-                        " lat:" + position.coords.latitude +
-                        " lon:" + position.coords.longitude
-                    );
-        // than use it to load from remote APIs some places nearby
-        showMessage("places is loading");
-        loadPlaces(position.coords)
-            .then((places) => {
-            showMessage("places was getted");
-                places.forEach((place) => {
-                    showMessage(
-                        "acc:" + position.coords.accuracy +
-                        " lat:" + position.coords.latitude +
-                        " lon:" + position.coords.longitude
-                    );
-                    console.log(position.coords);
-                    
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
+                    console.log("Your location: lat:" + latitude + ", long: " + longitude + "\n");
 
-                    // add place name
-                    const text = document.createElement('a-image');
-                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    text.setAttribute('title', place.name);
-                    text.setAttribute('src', 'assets/map-marker.png');
-//                     text.setAttribute('href', `https://www.google.ru/search?q=${place.name}`);
-                    text.setAttribute('scale', '20', '20');
+                    // add place icon
+                    const icon = document.createElement('a-image');
+                    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+                    icon.setAttribute('name', place.name);
+                    icon.setAttribute('src', 'assets/map-marker.png');
 
-                     text.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+                    // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+                    icon.setAttribute('scale', '20', '20');
+
+                    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
                     const clickListener = function(ev) {
                         ev.stopPropagation();
@@ -107,8 +87,9 @@ window.onload = () => {
                         }
                     };
 
-                    text.addEventListener('click', clickListener);                   
-                    scene.appendChild(text);
+                    icon.addEventListener('click', clickListener);
+                    
+                    scene.appendChild(icon);
                 });
             })
     },
@@ -120,6 +101,83 @@ window.onload = () => {
         }
     );
 };
+// window.onload = () => {
+//     const scene = document.querySelector('a-scene');
+//     return navigator.geolocation.getCurrentPosition(function (position) {
+//         getPlaces(position.coords)
+//             .then((places) => {
+//                 places.forEach((place) => {
+//     showMessage("Start get geopos");
+    
+
+//     // first get current user location
+//             showMessage("geops was getted");
+//          showMessage(
+//                         "acc:" + position.coords.accuracy +
+//                         " lat:" + position.coords.latitude +
+//                         " lon:" + position.coords.longitude
+//                     );
+//         // than use it to load from remote APIs some places nearby
+//         showMessage("places is loading");
+//         loadPlaces(position.coords)
+//             .then((places) => {
+//             showMessage("places was getted");
+//                 places.forEach((place) => {
+//                     showMessage(
+//                         "acc:" + position.coords.accuracy +
+//                         " lat:" + position.coords.latitude +
+//                         " lon:" + position.coords.longitude
+//                     );
+//                     console.log(position.coords);
+                    
+//                     const latitude = place.location.lat;
+//                     const longitude = place.location.lng;
+
+//                     // add place name
+//                     const text = document.createElement('a-image');
+//                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+//                     text.setAttribute('title', place.name);
+//                     text.setAttribute('src', 'assets/map-marker.png');
+// //                     text.setAttribute('href', `https://www.google.ru/search?q=${place.name}`);
+//                     text.setAttribute('scale', '20', '20');
+
+//                      text.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+//                     const clickListener = function(ev) {
+//                         ev.stopPropagation();
+//                         ev.preventDefault();
+
+//                         const name = ev.target.getAttribute('name');
+
+//                         const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+//                         if (el && el === ev.target) {
+//                             const label = document.createElement('span');
+//                             const container = document.createElement('div');
+//                             container.setAttribute('id', 'place-label');
+//                             label.innerText = name;
+//                             container.appendChild(label);
+//                             document.body.appendChild(container);
+
+//                             setTimeout(() => {
+//                                 container.parentElement.removeChild(container);
+//                             }, 1500);
+//                         }
+//                     };
+
+//                     text.addEventListener('click', clickListener);                   
+//                     scene.appendChild(text);
+//                 });
+//             })
+//     },
+//         (err) => console.error('Error in retrieving position', err),
+//         {
+//             enableHighAccuracy: true,
+//             maximumAge: 0,
+//             timeout: 27000,
+//         }
+//     );
+// };
 
 function showMessage(message)
 {
